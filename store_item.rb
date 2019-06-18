fc983e780080c63c8a1d5638787f811dd43b00a7
Sim <simchasitzer1@gmail.com>
2019-06-17 22:24:29 -0400

    made store_item.rb, add 3 items to it

---
+++ store_item.rb
@@ -0,0 +1,19 @@
+item1 = {
+  :name => "tiger",
+  :color => "orange",
+  :price => 5000
+}
+
+item2 = {
+  name: "bear",
+  color: "brown",
+  price: 8000
+}
+
+item3 = {
+  name: "snake",
+  color: "white",
+  price: 3000
+}
+
+p item3[:color]
