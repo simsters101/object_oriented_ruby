04e70a3341ddf454d80aef0ea3d7af811e773e43
Sim <simchasitzer1@gmail.com>
2019-06-18 20:48:07 -0400

    change input to input_options hash

--- store_item.rb
+++ store_item.rb
@@ -19,44 +19,31 @@
 # p item3[:color]
 
 class Item
-  def initialize(input_name, input_color, input_price)
-    @name = input_name
-    @color = input_color
-    @price = input_price
+  def initialize(input_options)
+    @name = input_options[:name]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
-  attr_reader :name
+  attr_reader :name, :color, :price
 
-  attr_writer :name
-
-  attr_reader :color
-
-  attr_writer :color
-
-  attr_reader :price
-  
-  attr_writer :price
+  attr_writer :name, :color, :price
 end
 
-item1 = Item.new("tiger", "orange", 5000)
-item2 = Item.new("bear", "brown", 8000)
-item3 = Item.new("snake", "white", 3000)
-
-puts item1.name
-puts item1.color
-puts item1.price
-
-item1.name = "girrafe"
-item1.color = "black"
-item1.price = 10000
-
-puts
-
+item1 = Item.new(name: "tiger", color: "orange", price: 5000)
+item2 = Item.new(name: "bear", color: "brown", price: 8000)
+item3 = Item.new(name: "snake", color: "white", price: 3000)
 puts item1.name
 puts item1.color
 puts item1.price
 
 puts
 
+puts item2.name
 puts item2.price
+item2.price = 16000
+puts item2.price
+
+puts
+
 puts item3.color
