43ef8f66e8ab97525e59f47fc18c04995bc24c3d
Sim <simchasitzer1@gmail.com>
2019-06-18 21:46:08 -0400

    make new file inheritance_example.rb, dry up Car and Bike class

---
+++ inheritance_example.rb
@@ -0,0 +1,36 @@
+class Car
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+
+class Bike < Car
+  def honk_horn
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+bike = Bike.new
+bike.honk_horn
+bike.ring_bell
